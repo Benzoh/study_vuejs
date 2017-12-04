154f4caccc8e94cdfc929a09e3db9c0e156d8a56
Mitsuhiro Wada <m.wada@kansai-kyodo.co.jp>
2017-12-04 14:57:02 +0900

    change: テンプレート構文終わり

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -74,3 +74,98 @@ var app7 = new Vue({
     ]
   }
 })
+
+// Vue インスタンス — Vue.js
+// https://jp.vuejs.org/v2/guide/instance.html
+
+// → コンポーネントツリーの詳細はVueのデブツールを見るとよくわかる
+
+// データとメソッド
+
+var data = {a: 1}
+
+var vm = new Vue({
+  data: data
+})
+
+// これらは同じオブジェクトを参照します
+vm.a === data.a
+console.log(vm.a === data.a)
+
+// プロパティへの代入は、元のデータにも反映
+vm.a = 2
+data.a
+console.log(data.a)
+
+// その逆も
+data.a = 3
+vm.a
+console.log(vm.a)
+
+
+// 新しいプロパティを追加する場合
+
+vm.b = 'hi'
+// b への変更はビューの更新を引き起こさない
+// 後でプロパティが必要になることがわかっているならば、空でも存在しない場合でも初期値を設定する
+
+// data: {
+//   newTodoText: '',
+//   visitCount: 0,
+//   hideCompletedTodos: false,
+//   todos: [],
+//   error: null
+// }
+
+// data プロパティに加えて、Vue インスタンスは、いくつかの便利なプロパティとメソッドを持っています。これらはユーザ定義のプロパティと区別するために、頭に $ が付けられています。
+
+var data = { a: 1 }
+var vm = new Vue({
+  el: '#example',
+  data: data
+})
+
+vm.$data === data
+console.log(vm.$data === data);
+vm.$el === document.getElementById('example')
+
+//  $watch はインスタンスメソッド
+vm.$watch('a', function(newValue, oldValue) {
+  // このコールバックは `vm.a` の値が変わる時に呼ばれます
+})
+
+
+// インスタンスライフサイクルフック
+
+new Vue({
+  data: {
+    a: 1
+  },
+  created: function() {
+    console.log('a is:' + this.a)
+  }
+})
+
+
+// 引数
+var app10 = new Vue({
+  el: '#app10',
+  methods: {
+    doSomething: function() {
+      console.log('start [doSomething]')
+    }
+  }
+})
+
+// 省略記法
+var app11 = new Vue({
+  el: '#app11',
+  data: {
+    url: 'https://www.benzoh.com/',
+  },
+  methods: {
+    doSomething: function() {
+      console.log('start [doSomething]')
+    }
+  }
+})
